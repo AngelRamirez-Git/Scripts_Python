5eda3c1a651a680af122f8189e1d6dfd40541cc3
Angel Ramírez <75238490+AngelRamirez-Git@users.noreply.github.com>
2024-07-01 00:03:01 -0600

    Add files via upload

---
+++ Spoof/detect_arp_spoof.py
@@ -0,0 +1,31 @@
+import subprocess
+import time
+
+# Dirección IP del router y su dirección MAC correcta
+ROUTER_IP = "192.168.207.1"
+CORRECT_MAC = "00:50:56:c0:00:01"
+
+def get_mac(ip):
+    try:
+        output = subprocess.check_output(["arp", "-n", ip])
+        for line in output.decode().split("\n"):
+            if ip in line:
+                return line.split()[2]
+    except Exception as e:
+        print(f"Error al obtener la MAC para {ip}: {e}")
+        return None
+
+def detect_arp_spoofing(router_ip, correct_mac):
+    current_mac = get_mac(router_ip)
+    if current_mac is None:
+        print(f"No se pudo obtener la MAC del router {router_ip}.")
+        return
+    if current_mac != correct_mac:
+        print(f"ALERTA: Se detectó un posible ataque ARP Spoofing. La dirección MAC actual {current_mac} no coincide con la dirección MAC correcta {correct_mac}.")
+    else:
+        print(f"Todo está bien. La dirección MAC del router {router_ip} no ha sido modificada.")
+
+if __name__ == "__main__":
+    while True:
+        detect_arp_spoofing(ROUTER_IP, CORRECT_MAC)
+        time.sleep(10)  # Verificar cada 10 segundos
